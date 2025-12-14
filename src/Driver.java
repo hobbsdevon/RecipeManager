829857de47a7f134bf0ab21c7f59cfcc76450210
hobbsdevon <hobbsdevonk@gmail.com>
2025-12-14 10:56:33 -0500

    Add multiple ingredients + general improvements to ingredients class

--- src/Driver.java
+++ src/Driver.java
@@ -1,20 +1,11 @@
 //package RecipeManager;
 
-import java.util.Scanner;
-
 public class Driver {
     public static void main(String[] args) {
-
-        Scanner scnr = new Scanner(System.in);
         
         System.out.println("Welcome to the Recipe Application!");
-
-        String ingredient = "";
-        System.out.println("Let's add an ingredient. Please enter the ingredient name: ");
-        ingredient = scnr.nextLine();
-        Ingredient.addIngredient(ingredient);
-
-        scnr.close();
+        
+        Ingredient.addIngredient();
         return;
     }
 }
--- src/Ingredient.java
+++ src/Ingredient.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Ingredient {
 
-    public static Ingredient addIngredient(String ingredient) {
+    public static Ingredient addIngredient() {
         Scanner scnr = new Scanner(System.in);
 
         String nameOfIngredient = "";
@@ -12,31 +12,50 @@ public class Ingredient {
         String unitMeasurement = "";
         float numberCaloriesPerCup = 0;
         float totalCalories = 0.0f;
+        boolean moreIngredients = true;
 
-        nameOfIngredient = ingredient;
+        while(moreIngredients == true) { // Loop to add multiple ingredients
+            System.out.println("Let's add an ingredient. Please enter the ingredient name: ");
+                    if(scnr.hasNextLine()){
+                            nameOfIngredient = scnr.nextLine();
+                    }
+                    //System.out.println("The value of nameOfIngredient is: " + nameOfIngredient);
 
-        System.out.println("Please enter the unit of measurement for "
-                + nameOfIngredient + ": ");
-                if(scnr.hasNext()){
-                        unitMeasurement = scnr.next();
-                }
-        
+            System.out.println("Please enter the unit of measurement for "
+                    + nameOfIngredient + ": ");
+                    if(scnr.hasNextLine()){
+                            unitMeasurement = scnr.nextLine();
+                    }
+            
 
-        System.out.println("Please enter the number of " + unitMeasurement + " of "
-                + nameOfIngredient + " we'll need: ");
-                if(scnr.hasNextFloat()){
-                        ingredientAmount = scnr.nextFloat();
-                }
+            System.out.println("Please enter the number of " + unitMeasurement + " of "
+                    + nameOfIngredient + " we'll need: ");
+                    if(scnr.hasNextFloat()){
+                            ingredientAmount = scnr.nextFloat();
+                    }
 
-        System.out.println("Please enter the name of calories per cup: ");
-                if(scnr.hasNextFloat()){
-                        numberCaloriesPerCup = scnr.nextFloat();
-                }
+            System.out.println("Please enter the name of calories per " + unitMeasurement + ": ");
+                    if(scnr.hasNextFloat()){
+                            numberCaloriesPerCup = scnr.nextFloat();
+                    }
 
-        totalCalories = ingredientAmount * numberCaloriesPerCup;
+            totalCalories = ingredientAmount * numberCaloriesPerCup;
 
-        System.out.println(nameOfIngredient + " uses " + ingredientAmount + " "
-                + unitMeasurement + " and has " + totalCalories + " calories.");
+            System.out.println(nameOfIngredient + " uses " + ingredientAmount + " "
+                    + unitMeasurement + " and has " + totalCalories + " calories.");
+
+            System.out.println("Would you like to add another ingredient? (y/n): ");
+
+            char userChoice = scnr.next().charAt(0);
+            scnr.nextLine(); // Consume the newline character
+            if(userChoice == 'y' || userChoice == 'Y'){
+                    moreIngredients = true;
+            }
+            else{
+                    moreIngredients = false;
+            }
+            //System.out.println("The value of moreIngredients is: " + moreIngredients);
+        }      
 
         scnr.close();        
         return null;
