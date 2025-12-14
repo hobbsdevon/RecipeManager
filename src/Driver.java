f74a362bf9feec2a6fae1f268fd46d8a8fd4ef91
hobbsdevon <hobbsdevonk@gmail.com>
2025-12-14 11:17:41 -0500

    Modfied addIngredient to return ingredient. Set Ingredient attributes & added getter functions

--- src/Driver.java
+++ src/Driver.java
@@ -1,11 +1,22 @@
 //package RecipeManager;
 
+import java.util.Scanner;
+ 
 public class Driver {
     public static void main(String[] args) {
-        
+        Scanner scnr = new Scanner(System.in);
+
         System.out.println("Welcome to the Recipe Application!");
-        
-        Ingredient.addIngredient();
-        return;
+
+        Ingredient ingredient1 = Ingredient.addIngredient();
+
+        // Print the ingredient's attributes
+        System.out.println("\nIngredient Details:");
+        System.out.println("Name: " + ingredient1.getName());
+        System.out.println("Amount: " + ingredient1.getAmount());
+        System.out.println("Unit: " + ingredient1.getUnit());
+        System.out.println("Total Calories: " + ingredient1.getTotalCalories());
+
+        scnr.close();
     }
 }
--- src/Ingredient.java
+++ src/Ingredient.java
@@ -4,61 +4,75 @@ import java.util.Scanner;
 
 public class Ingredient {
 
+    private String name;
+    private float amount;
+    private String unit;
+    private float totalCalories;
+
+    //Getter methods
+    public String getName() {
+        return name;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public float getTotalCalories() {
+        return totalCalories;
+    }
+    /* 
+    addIngredient takes user input for name, amount, unit measurement, and calories,
+    and returns an Ingredient object with those attributes.
+    */
     public static Ingredient addIngredient() {
         Scanner scnr = new Scanner(System.in);
+
+        Ingredient ingredient = new Ingredient();
 
         String nameOfIngredient = "";
         float ingredientAmount = 0.0f;
         String unitMeasurement = "";
-        float numberCaloriesPerCup = 0;
+        float numberCaloriesPerUnit = 0;
         float totalCalories = 0.0f;
-        boolean moreIngredients = true;
 
-        while(moreIngredients == true) { // Loop to add multiple ingredients
-            System.out.println("Let's add an ingredient. Please enter the ingredient name: ");
-                    if(scnr.hasNextLine()){
-                            nameOfIngredient = scnr.nextLine();
-                    }
-                    //System.out.println("The value of nameOfIngredient is: " + nameOfIngredient);
+        System.out.println("Let's add an ingredient. Please enter the ingredient name: ");
+        if (scnr.hasNextLine()) {
+            nameOfIngredient = scnr.nextLine();
+        }
 
-            System.out.println("Please enter the unit of measurement for "
-                    + nameOfIngredient + ": ");
-                    if(scnr.hasNextLine()){
-                            unitMeasurement = scnr.nextLine();
-                    }
-            
+        System.out.println("Please enter the unit of measurement for " + nameOfIngredient + ": ");
+        if (scnr.hasNextLine()) {
+            unitMeasurement = scnr.nextLine();
+        }
 
-            System.out.println("Please enter the number of " + unitMeasurement + " of "
-                    + nameOfIngredient + " we'll need: ");
-                    if(scnr.hasNextFloat()){
-                            ingredientAmount = scnr.nextFloat();
-                    }
+        System.out.println("Please enter the number of " + unitMeasurement + " of " + nameOfIngredient + " we'll need: ");
+        if (scnr.hasNextFloat()) {
+            ingredientAmount = scnr.nextFloat();
+        }
 
-            System.out.println("Please enter the name of calories per " + unitMeasurement + ": ");
-                    if(scnr.hasNextFloat()){
-                            numberCaloriesPerCup = scnr.nextFloat();
-                    }
+        System.out.println("Please enter the calories per " + unitMeasurement + ": ");
+        if (scnr.hasNextFloat()) {
+            numberCaloriesPerUnit = scnr.nextFloat();
+        }
 
-            totalCalories = ingredientAmount * numberCaloriesPerCup;
+        totalCalories = ingredientAmount * numberCaloriesPerUnit;
 
-            System.out.println(nameOfIngredient + " uses " + ingredientAmount + " "
-                    + unitMeasurement + " and has " + totalCalories + " calories.");
+        // Set the fields on the ingredient object
+        ingredient.name = nameOfIngredient;
+        ingredient.amount = ingredientAmount;
+        ingredient.unit = unitMeasurement;
+        ingredient.totalCalories = totalCalories;
 
-            System.out.println("Would you like to add another ingredient? (y/n): ");
+        System.out.println(nameOfIngredient + " uses " + ingredientAmount + " " 
+                + unitMeasurement + " and has " + totalCalories + " calories.");
 
-            char userChoice = scnr.next().charAt(0);
-            scnr.nextLine(); // Consume the newline character
-            if(userChoice == 'y' || userChoice == 'Y'){
-                    moreIngredients = true;
-            }
-            else{
-                    moreIngredients = false;
-            }
-            //System.out.println("The value of moreIngredients is: " + moreIngredients);
-        }      
-
-        scnr.close();        
-        return null;
+        scnr.close();
+        return ingredient;
     }
 
 }
